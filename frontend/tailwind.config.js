c62bffa6fc766117ad27c5c96511f479c5c43d7b
Ibuchukwu Umeano <ibumeano@gmail.com>
2024-02-19 16:38:19 +0100

    Initial commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: ["./src/components/**/*.tsx","./src/pages/**/*.tsx", "./src/*.{html,tsx}", "./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
