bd54567c5e23014a1efa51ab0613982dd9f74bda
Ibuchukwu Umeano <ibumeano@gmail.com>
2024-03-05 03:40:04 +0100

    ...

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,8 @@ app.use(
   cors({
     origin: [
       "https://proj-dash.vercel.app",
-      "https://proj-lake-seven.vercel.app", "http://localhost:5173"
+      "https://proj-lake-seven.vercel.app",
+      "http://localhost:5173",
     ],
     credentials: true,
   })
@@ -29,15 +30,23 @@ app.get("/", (req, res) => {
 // Routing
 app.post("/confirmRedirect", (req, res) => {
   const { idToken } = req.body;
-  res.cookie('firebaseAuthToken', idToken, {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  ); // Allowed HTTP methods
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allowed headers
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.cookie("firebaseAuthToken", idToken, {
+    domain: ".vercel.app",
     maxAge: 24 * 60 * 60 * 1000, // 24 hours in milliseconds
     expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 24 hours in milliseconds
     secure: true, // Only send the cookie over HTTPS
-    sameSite: 'none' // Allow cross-site usage
+    sameSite: "none", // Allow cross-site usage
   });
 
   // Redirect to the desired URL
-  res.redirect(301,"https://proj-dash.vercel.app");
+  res.redirect(301, "https://proj-dash.vercel.app");
 });
 
 app.use("/auth", authRoutes);
